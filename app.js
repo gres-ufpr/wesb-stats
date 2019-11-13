f8d5ba48bb59bffa40323fd6b0babe9ba5f9341f
Thiago Nascimento <thiagodnf@gmail.com>
2019-11-13 13:21:53 -0300

    updated packages

--- app.js
+++ app.js
@@ -6,31 +6,46 @@ var connect = require('connect');
     liveReload = require('livereload');
     project = require('./package.json');
 
+    // lets create a global logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ]
+});
+
 connect().use('/' + project.name, serveStatic(__dirname)).listen(port)
 
 var liveReloadServer = liveReload.createServer()
 
 liveReloadServer.watch(__dirname)
 
-winston.info('Running:')
-winston.info('\t' + project.name)
-winston.info('LiveReload Server is watching:')
-winston.info('\t' + __dirname)
+logger.info('Running:')
+logger.info('\t' + project.name)
+logger.info('LiveReload Server is watching:')
+logger.info('\t' + __dirname)
 
 network.get_active_interface(function(err, obj) {
 
     var address = `:${port}/${project.name}`;
 
-    winston.info('The magic happens at')
-    winston.info('\t Localhost: http://localhost' + address)
+    logger.info('The magic happens at')
+    logger.info('\t Localhost: http://localhost' + address)
 
     network.get_public_ip(function(err, ip) {
         // should return your public IP address
-        winston.info("\t Public IP:", err || ip + address);
+        logger.info("\t Public IP:", err || ip + address);
     })
 
     network.get_private_ip(function(err, ip) {
         // err may be 'No active network interface found'.
-        winston.info("\t Private IP:", err || ip + address);
+        logger.info("\t Private IP:", err || ip + address);
     })
 });
